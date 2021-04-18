f1357b14d4e5b75ab4f6835ec4636b7e1240fe4d
ewilso <17hughwilson@gmail.com>
2021-04-18 15:53:44 +0100

    Funky Town

--- main.py
+++ main.py
@@ -4,40 +4,43 @@ import datetime
 import os
 import time
 import pyaudio
+import random
 
 def run(command):
     os.system(command)
     os.system("aplay bleep.wav")
 
 def speak(text):
-    tts = gtts.gTTS(text)
-    tts.save("say.mp3")
-    os.system("mpg123 -q say.mp3")
+    if text == "Welcome Sequence":
+        os.system("mpg123 -q lines/prehub06.mp3")
+    else:
+        os.system("echo " + text + " | festival --tts")
+    #os.system("mpg123 -q say.mp3")
 
 def greeting():
-    hour=datetime.datetime.now().hour
-    if hour>=0 and hour<12:
-        speak("Good morning. How can I help you?")
-    elif hour>=12 and hour<18:
-        speak("Good afternoon. How can I help you?")
-    else:
-        speak("Good evening. How can I help you?")
+    path="/home/wilson/lib/portal/Cave Johnson/PTI/"
+    files=os.listdir(path)
+    d=random.choice(files)
+    os.system("mpg123 -q /home/wilson/lib/portal/Cave\ Johnson/PTI/" + d)
+    print("--------------------------------------------------------------------")
 
 def takeCommand():
-    data = stream.read(4000)
+    data = stream.read(700)
     if len(data) == 0:
         return ""
     if rec.AcceptWaveform(data):
         stage1 = rec.Result()
         stage2 = stage1.split('"text" : "')[1]
         result = stage2.split('"\n}')[0]
-        print(result)
+        if result != "":
+            print(result)
         return result
     else:
         return ""
 
 if __name__=='__main__':
-    speak("Booting up now")
+    speak("Welcome Sequence")
+    print("--------------------------------------------------------------------")
     if not os.path.exists("model"):
         print ("Model needed, but not found.")
         exit (1)
@@ -56,21 +59,24 @@ if __name__=='__main__':
         statement = takeCommand()
         if statement=="":
             continue
-        if "open" in statement or "load" in statement:
-            if "term" in statement:
-                run("st &")
-            if "browser" in statement or " brave" in statement:
-                run("brave &")
-            if "atom" in statement or "editor" in statement:
-                run("atom &")
-        if " lock " in statement:
-            run("slock &")
-        if ("create" in statement or "make" in statement or "start" in statement) and "project" in statement:
-            run("")
-            name = ""
-            while name == "":
-                name = takeCommand()
-            run("cd ~/lib/code && mkdir " + name)
-        if "bye" in statement:
-            speak("Goodbye")
-            break
+        else:
+            if "open" in statement or "load" in statement:
+                if "term" in statement:
+                    run("st &")
+                if "browser" in statement or " brave" in statement:
+                    run("brave &")
+                if "atom" in statement or "editor" in statement:
+                    run("atom &")
+            elif " lock " in statement:
+                run("slock &")
+            elif "drop my needle" in statement:
+                run("cd ~/lib/music && mpv * &")
+            if ("create" in statement or "make" in statement or "start" in statement) and "project" in statement:
+                name = ""
+                while name == "":
+                    name = takeCommand()
+                run("cd ~/lib/code && mkdir " + name)
+            elif "bye" in statement:
+                speak("Goodbye")
+                print("--------------------------------------------------------------------")
+                break
